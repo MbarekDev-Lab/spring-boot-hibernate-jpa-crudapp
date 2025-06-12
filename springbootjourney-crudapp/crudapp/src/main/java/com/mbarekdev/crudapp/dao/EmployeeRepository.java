fee9f6294f666fa4cb28343d5e05358971d914b3
M'barek Benraiss <81750067+MbarekDev-Lab@users.noreply.github.com>
2025-06-12 07:26:09 +0200

    Spring Data JPA

--- springbootjourney-crudapp/crudapp/src/main/java/com/mbarekdev/crudapp/dao/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mbarekdev.crudapp.dao;
-
-import com.mbarekdev.crudapp.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
