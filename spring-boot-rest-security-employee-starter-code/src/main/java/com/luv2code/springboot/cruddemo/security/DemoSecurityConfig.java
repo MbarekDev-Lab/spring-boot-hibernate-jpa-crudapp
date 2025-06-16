181bbb164b8b8b1e918a2d16e8f50f8e94a56c31
M'barek Benraiss <81750067+MbarekDev-Lab@users.noreply.github.com>
2025-06-16 08:01:41 +0200

    configures Spring Security for your REST API

--- spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -2,6 +2,8 @@ package com.luv2code.springboot.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,7 +12,6 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
-
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
 
@@ -36,11 +37,22 @@ public class DemoSecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(config -> config
-                .requestMatchers("/api/employees/**").hasRole("EMPLOYEE")
-                .anyRequest().authenticated()
-        ).httpBasic();
-        return http.build();
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(configierer ->
+                configierer
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+        // use HTTP Basic authentication
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        // disable Cross Site Request Forgery  (CSRF)
+        // in general, not requered for statless REST APIs use POST, PUT, DELETE and/or PATCH
+        httpSecurity.csrf(csrf -> csrf.disable());
+
+        return httpSecurity.build();
     }
+
 }
