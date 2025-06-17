3e30412d8842de2801628506a49029ed6a6d5be2
M'barek Benraiss <81750067+MbarekDev-Lab@users.noreply.github.com>
2025-06-17 22:11:02 +0200

    secure REST API using a custom database-based user system (instead of hardcoded users)

--- spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -5,16 +5,66 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
+    // add support for JDBC ... no more hardcoded users ;>
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from members where user_id =?");
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id , role from roles where user_id =?");
+        return jdbcUserDetailsManager;
+    }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(configierer ->
+                configierer
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PATCH, "/api/employees/**").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+        // use HTTP Basic authentication
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        // disable Cross Site Request Forgery (CSRF)
+        // in general, not requered for statless REST APIs use POST, PUT, DELETE and/or PATCH
+        httpSecurity.csrf(csrf -> csrf.disable());
+
+        return httpSecurity.build();
+    }
+
+    /*@Transaction
+    public void incrementOrInsert(String barcode) {
+        ProductScan existing = dao.getByBarcode(barcode);
+        if (existing != null) {
+            existing.count += 1;
+            existing.needsUpload = true; // ← KEY
+            dao.update(existing);
+        } else {
+            ProductScan newScan = new ProductScan();
+            newScan.productBarcode = barcode;
+            newScan.count = 1;
+            newScan.timestamp = System.currentTimeMillis();
+            newScan.needsUpload = true;
+            dao.insert(newScan);
+        }
+    }*/
+
+
+
+    /* @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
         UserDetails mbarek = User.builder()
                 .username("mbarek")
                 .password("{noop}test123")
@@ -34,26 +84,6 @@ public class DemoSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(mbarek, mary, susan);
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests(configierer ->
-                configierer
-                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.PATCH, "/api/employees/**").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
-        );
-        // use HTTP Basic authentication
-        httpSecurity.httpBasic(Customizer.withDefaults());
-        // disable Cross Site Request Forgery  (CSRF)
-        // in general, not requered for statless REST APIs use POST, PUT, DELETE and/or PATCH
-        httpSecurity.csrf(csrf -> csrf.disable());
-
-        return httpSecurity.build();
-    }
+    }*/
 
 }
