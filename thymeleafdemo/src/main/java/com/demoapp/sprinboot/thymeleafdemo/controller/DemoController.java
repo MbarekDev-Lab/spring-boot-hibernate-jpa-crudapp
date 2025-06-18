14732d9825756f22a83e088d266e1aa5cc10acc8
M'barek Benraiss <81750067+MbarekDev-Lab@users.noreply.github.com>
2025-06-18 07:46:01 +0200

    Spring MVC with Thymeleaf and css

--- thymeleafdemo/src/main/java/com/demoapp/sprinboot/thymeleafdemo/controller/DemoController.java
+++ thymeleafdemo/src/main/java/com/demoapp/sprinboot/thymeleafdemo/controller/DemoController.java
@@ -1,16 +1,15 @@
 package com.demoapp.sprinboot.thymeleafdemo.controller;
-
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class DemoController {
-    // create a mapping for "/hello"
 
     @GetMapping("/hello")
     public String sayHello(Model theModel) {
         theModel.addAttribute("theDate", java.time.LocalDateTime.now());
 
-        return "showTime";
+        return "showTime"; // showTime.html should be inside src/main/resources/templates/
     }
-
 }
