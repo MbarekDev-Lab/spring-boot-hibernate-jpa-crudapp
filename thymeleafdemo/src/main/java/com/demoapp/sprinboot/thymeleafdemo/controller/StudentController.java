3ec9697e213bcbdb79fdf090de56369354bff1cd
M'barek Benraiss <81750067+MbarekDev-Lab@users.noreply.github.com>
2025-06-21 14:58:07 +0200

    binding a group of radio buttons to a single field . Thymeleaf and Spring will automatically map the selected radio button to model object on form submission.

--- thymeleafdemo/src/main/java/com/demoapp/sprinboot/thymeleafdemo/controller/StudentController.java
+++ thymeleafdemo/src/main/java/com/demoapp/sprinboot/thymeleafdemo/controller/StudentController.java
@@ -1,29 +1,39 @@
 package com.demoapp.sprinboot.thymeleafdemo.controller;
 
 import com.demoapp.sprinboot.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+    @Value("${countries}")
+    private List<String> countries;
 
+    // @RequestMapping("/processStudentForm") // This maps both GET and POST by default
+    // @PostMapping("/processStudentForm") // Accepts only POST
     @GetMapping("/showStudentForm")
     public String showForm(Model theModel) {
         // Create a student object
         Student theStudent = new Student();
         // Add a student object to the model
         theModel.addAttribute("student", theStudent);
+        // add a List of Countries to the Model
+        theModel.addAttribute("countries", countries);
         return "student-form";  // Thymeleaf template name
     }
 
-    @PostMapping("/processStudentForm")
+    /*@PostMapping("/processStudentForm")
     public String processStudentForm(@ModelAttribute("student") Student theStudent) {
         // Log the input data to console
-        System.out.println("Student: " + theStudent.getFirstName() + " " + theStudent.getLastName());
+        System.out.println("Student: " + theStudent.getFirstName() + " " + theStudent.getLastName() + " from " + theStudent.getCountry());
+        return "student-confirmation";  //Confirmation page template name
+    }*/
 
-        return "student-confirmation";  // Confirmation page template name
-    }
+
 }
--- thymeleafdemo/src/main/java/com/demoapp/sprinboot/thymeleafdemo/model/Student.java
+++ thymeleafdemo/src/main/java/com/demoapp/sprinboot/thymeleafdemo/model/Student.java
@@ -1,11 +1,12 @@
 package com.demoapp.sprinboot.thymeleafdemo.model;
 
 public class Student {
-    private String firstName ;
-    private String lastName ;
+    private String firstName;
+    private String lastName;
+    private String country;
+    private String favoriteLanguage;
 
     public Student() {
-
     }
 
     public String getFirstName() {
@@ -23,4 +24,21 @@ public class Student {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getFavoriteLanguage() {
+        return favoriteLanguage;
+    }
+
+    public void setFavoriteLanguage(String favoriteLanguage) {
+        this.favoriteLanguage = favoriteLanguage;
+    }
+
 }
