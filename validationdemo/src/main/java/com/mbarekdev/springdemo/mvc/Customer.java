9e1a02194a924218b52d61598b84c7f2d0f555b0
M'barek Benraiss <81750067+MbarekDev-Lab@users.noreply.github.com>
2025-06-22 10:02:31 +0200

    Spring MVC Validation Free passes

--- validationdemo/src/main/java/com/mbarekdev/springdemo/mvc/Customer.java
+++ validationdemo/src/main/java/com/mbarekdev/springdemo/mvc/Customer.java
@@ -1,14 +1,19 @@
 package com.mbarekdev.springdemo.mvc;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class Customer {
     private String firstName;
-
     @NotNull(message = "is required")
-    @Size(min =1 , message = "is required")
+    @Size(min = 1, message = "is required")
     private String lastName = "";
+
+    @Min(value = 0, message = "must be greater than or equal to zero ")
+    @Max(value = 10, message = "must be greater than or equal to 10 ")
+    private int freePasses;
 
     public String getFirstName() {
         return firstName;
@@ -24,5 +29,13 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
     }
 }
