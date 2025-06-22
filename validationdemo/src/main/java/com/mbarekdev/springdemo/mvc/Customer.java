db4e5fcd3e2ba246e58b3a8a8ae77da66d46d7aa
M'barek Benraiss <81750067+MbarekDev-Lab@users.noreply.github.com>
2025-06-22 12:08:28 +0200

    custome error names

--- validationdemo/src/main/java/com/mbarekdev/springdemo/mvc/Customer.java
+++ validationdemo/src/main/java/com/mbarekdev/springdemo/mvc/Customer.java
@@ -10,7 +10,7 @@ public class Customer {
 
     @Min(value = 0, message = "must be greater than or equal to zero ")
     @Max(value = 10, message = "must be greater than or equal to 10 ")
-    private int freePasses;
+    private Integer freePasses;
 
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
     private String postalCode;
@@ -31,11 +31,11 @@ public class Customer {
         this.lastName = lastName;
     }
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 
