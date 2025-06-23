fb244b96851b67b3478a397bcdebbd6e83ea69be
M'barek Benraiss <81750067+MbarekDev-Lab@users.noreply.github.com>
2025-06-23 08:31:26 +0200

    CRUD

---
+++ spring-boot-spring-mvc-crud/spring-boot-spring-mvc-crud-starter-code/src/main/java/com/mbarekDev/springboot/cruddemo/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.mbarekDev.springboot.cruddemo.service;
+
+import com.mbarekDev.springboot.cruddemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
+
+}
