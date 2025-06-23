69adf2f4f3398962bd9c20e2d2774f2be687f1d0
M'barek Benraiss <81750067+MbarekDev-Lab@users.noreply.github.com>
2025-06-23 08:26:39 +0200

    Spring MVC CRUD

--- validationdemo/src/main/java/com/mbarekdev/springdemo/mvc/Customer.java
+++ validationdemo/src/main/java/com/mbarekdev/springdemo/mvc/Customer.java
@@ -19,7 +19,6 @@ public class Customer {
     @CourseCode(value = "MbarekDev", message = "Must start with MbarekDev")
     private String courseCode;
 
-
     public String getFirstName() {
         return firstName;
     }
