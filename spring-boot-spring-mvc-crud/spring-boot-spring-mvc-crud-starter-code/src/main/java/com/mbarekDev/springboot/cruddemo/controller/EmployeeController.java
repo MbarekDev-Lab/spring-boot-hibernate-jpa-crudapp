45b441b3efc5210f7186cc405e603a0ac665b64b
M'barek Benraiss <81750067+MbarekDev-Lab@users.noreply.github.com>
2025-06-23 08:44:06 +0200

    Spring MVC CRUD

---
+++ spring-boot-spring-mvc-crud/spring-boot-spring-mvc-crud-starter-code/src/main/java/com/mbarekDev/springboot/cruddemo/controller/EmployeeController.java
@@ -0,0 +1,33 @@
+package com.mbarekDev.springboot.cruddemo.controller;
+
+import com.mbarekDev.springboot.cruddemo.entity.Employee;
+import com.mbarekDev.springboot.cruddemo.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService theEmployeeService){
+        employeeService = theEmployeeService;
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model theModel){
+        //get the employees from db;
+        List<Employee> theEmployees = employeeService.findAll();
+
+        //add that to the spring model
+        theModel.addAttribute("employees",theEmployees);
+        return "list-employees";
+    }
+
+
+
+}
