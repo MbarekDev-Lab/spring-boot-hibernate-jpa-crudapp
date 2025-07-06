8202570b66b306d92e8a821c59a3bbaad8876d22
Mbarek Benraiss <81750067+malika-1989@users.noreply.github.com>
2025-07-06 21:14:49 +0200

    @ManyToMany  oneToMany Uni-direction

---
+++ spring-boot-jpa-advanced-mappings/jpa-one-to-many-uni/src/main/java/com/mbarekDev/cruddemo/entity/Review.java
@@ -0,0 +1,47 @@
+package com.mbarekDev.cruddemo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "review")
+public class Review {
+    // define fields
+    // define conistructor
+    // define getter / setter
+    // define toString
+    // annotation fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String comment;
+
+    public Review(String comment) {
+        this.comment = comment;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name = "comment")
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "id=" + id +
+                ", comment='" + comment + '\'' +
+                '}';
+    }
+}
